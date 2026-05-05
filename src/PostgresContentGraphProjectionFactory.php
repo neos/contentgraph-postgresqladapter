mhsdesign/Flowpack.ContentGraph.PostgreSQLAdapter

clean up: properly register postgres types at setup time without workaround

src/PostgresContentGraphProjectionFactory.php
@@ -6,6 +6,7 @@ namespace Flowpack\ContentGraph\PostgreSQLAdapter;
 
 use Doctrine\DBAL\Connection;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\PostgresContentGraphProjection;
+use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\SchemaBuilder\HypergraphSchemaBuilder;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Repository\NodeFactory;
 use Neos\ContentRepository\Core\Factory\SubscriberFactoryDependencies;
 use Neos\ContentRepository\Core\Projection\ContentGraph\ContentGraphProjectionFactoryInterface;
@@ -23,6 +24,10 @@ final readonly class PostgresContentGraphProjectionFactory implements ContentGra
     public function build(
         SubscriberFactoryDependencies $projectionFactoryDependencies,
     ): PostgresContentGraphProjection {
+        // Register custom Doctrine types early so they are available for all
+        // subsequent schema operations (setUp, reset, status, ...).
+        HypergraphSchemaBuilder::registerTypes($this->dbal);
+
         $nodeFactory = new NodeFactory(
             $projectionFactoryDependencies->contentRepositoryId,
             $projectionFactoryDependencies->getPropertyConverter()

neos/contentgraph-postgresqladapter

src/PostgresContentGraphProjectionFactory.php
@@ -6,6 +6,7 @@ namespace Neos\ContentGraph\PostgreSQLAdapter;
 
 use Doctrine\DBAL\Connection;
 use Neos\ContentGraph\PostgreSQLAdapter\Domain\Projection\PostgresContentGraphProjection;
+use Neos\ContentGraph\PostgreSQLAdapter\Domain\Projection\SchemaBuilder\HypergraphSchemaBuilder;
 use Neos\ContentGraph\PostgreSQLAdapter\Domain\Repository\NodeFactory;
 use Neos\ContentRepository\Core\Factory\SubscriberFactoryDependencies;
 use Neos\ContentRepository\Core\Projection\ContentGraph\ContentGraphProjectionFactoryInterface;
@@ -23,6 +24,10 @@ final readonly class PostgresContentGraphProjectionFactory implements ContentGra
     public function build(
         SubscriberFactoryDependencies $projectionFactoryDependencies,
     ): PostgresContentGraphProjection {
+        // Register custom Doctrine types early so they are available for all
+        // subsequent schema operations (setUp, reset, status, ...).
+        HypergraphSchemaBuilder::registerTypes($this->dbal);
+
         $nodeFactory = new NodeFactory(
             $projectionFactoryDependencies->contentRepositoryId,
             $projectionFactoryDependencies->getPropertyConverter()