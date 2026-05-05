mhsdesign/Flowpack.ContentGraph.PostgreSQLAdapter

WIP - trying to fix removal. Nodes are removed, but orphaned nodes

src/Domain/Projection/HierarchyRelationRecord.php
@@ -16,6 +16,7 @@ namespace Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception as DBALException;
+use Flowpack\ContentGraph\PostgreSQLAdapter\ContentGraphTableNames;
 use Neos\ContentRepository\Core\DimensionSpace\DimensionSpacePoint;
 use Neos\ContentRepository\Core\SharedModel\Workspace\ContentStreamId;
 
@@ -31,18 +32,18 @@ final readonly class HierarchyRelationRecord
         public ContentStreamId $contentStreamId,
         public NodeRelationAnchorPoint $parentNodeAnchor,
         public DimensionSpacePoint $dimensionSpacePoint,
-        public NodeRelationAnchorPoints $childNodeAnchorPoints
+        public NodeRelationAnchorPoints $childNodeAnchors
     ) {
     }
 
     /**
-     * @param array<string,string> $databaseRow
+     * @param array<string,mixed> $databaseRow
      */
     public static function fromDatabaseRow(array $databaseRow): self
     {
         return new self(
             ContentStreamId::fromString($databaseRow['contentstreamid']),
-            NodeRelationAnchorPoint::fromString($databaseRow['parentnodeanchor']),
+            NodeRelationAnchorPoint::fromInteger($databaseRow['parentnodeanchor']),
             DimensionSpacePoint::fromJsonString($databaseRow['dimensionspacepoint']),
             NodeRelationAnchorPoints::fromDatabaseString(
                 $databaseRow['childnodeanchors']
@@ -62,4 +63,51 @@ final readonly class HierarchyRelationRecord
         ];
     }
 
+    public function removeChildNodeAnchor(
+        NodeRelationAnchorPoint $childNodeAnchor,
+        Connection $databaseConnection,
+        ContentGraphTableNames $tableNames
+    ): void {
+        $tableName = $tableNames->hierarchyRelation();
+        $id = $this->getDatabaseIdentifier();
+        try {
+            $databaseConnection->executeStatement(
+                <<<SQL
+                    WITH updated AS (
+                        UPDATE {$tableName}
+                        SET childnodeanchors = array_remove(childnodeanchors, :childnodeanchor_to_remove),
+                            subtreetags = subtreetags - :childnodeanchor_to_remove_text
+                        WHERE contentstreamid = :contentstreamid
+                          AND parentnodeanchor = :parentnodeanchor
+                          AND dimensionspacepointhash = :dimensionspacepointhash
+                        RETURNING contentstreamid, parentnodeanchor, dimensionspacepointhash, childnodeanchors
+                    )
+                    DELETE FROM {$tableName} h
+                    USING updated AS u
+                    WHERE h.contentstreamid = u.contentstreamid
+                      AND h.parentnodeanchor = u.parentnodeanchor
+                      AND h.dimensionspacepointhash = u.dimensionspacepointhash
+                      AND array_length(u.childnodeanchors, 1) IS NULL
+                SQL,
+                [
+                    'contentstreamid' => $id['contentstreamid'],
+                    'parentnodeanchor' => $id['parentnodeanchor'],
+                    'dimensionspacepointhash' => $id['dimensionspacepointhash'],
+                    'childnodeanchor_to_remove' => $childNodeAnchor->value,
+                    'childnodeanchor_to_remove_text' => (string)$childNodeAnchor->value,
+                ]
+            );
+        } catch (DBALException $e) {
+            throw new \RuntimeException(sprintf('Failed to remove child node anchor from hierarchy relation: %s', $e->getMessage()), 1716484900, $e);
+        }
+    }
+
+    public function removeFromDatabase(Connection $databaseConnection, ContentGraphTableNames $tableNames): void
+    {
+        try {
+            $databaseConnection->delete($tableNames->hierarchyRelation(), $this->getDatabaseIdentifier());
+        } catch (DBALException $e) {
+            throw new \RuntimeException(sprintf('Failed to remove hierarchy relation from database: %s', $e->getMessage()), 1716484910, $e);
+        }
+    }
 }

neos/contentgraph-postgresqladapter

src/Domain/Projection/HierarchyRelationRecord.php
@@ -16,6 +16,7 @@ namespace Neos\ContentGraph\PostgreSQLAdapter\Domain\Projection;
 
 use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception as DBALException;
+use Neos\ContentGraph\PostgreSQLAdapter\ContentGraphTableNames;
 use Neos\ContentRepository\Core\DimensionSpace\DimensionSpacePoint;
 use Neos\ContentRepository\Core\SharedModel\Workspace\ContentStreamId;
 
@@ -31,18 +32,18 @@ final readonly class HierarchyRelationRecord
         public ContentStreamId $contentStreamId,
         public NodeRelationAnchorPoint $parentNodeAnchor,
         public DimensionSpacePoint $dimensionSpacePoint,
-        public NodeRelationAnchorPoints $childNodeAnchorPoints
+        public NodeRelationAnchorPoints $childNodeAnchors
     ) {
     }
 
     /**
-     * @param array<string,string> $databaseRow
+     * @param array<string,mixed> $databaseRow
      */
     public static function fromDatabaseRow(array $databaseRow): self
     {
         return new self(
             ContentStreamId::fromString($databaseRow['contentstreamid']),
-            NodeRelationAnchorPoint::fromString($databaseRow['parentnodeanchor']),
+            NodeRelationAnchorPoint::fromInteger($databaseRow['parentnodeanchor']),
             DimensionSpacePoint::fromJsonString($databaseRow['dimensionspacepoint']),
             NodeRelationAnchorPoints::fromDatabaseString(
                 $databaseRow['childnodeanchors']
@@ -62,4 +63,51 @@ final readonly class HierarchyRelationRecord
         ];
     }
 
+    public function removeChildNodeAnchor(
+        NodeRelationAnchorPoint $childNodeAnchor,
+        Connection $databaseConnection,
+        ContentGraphTableNames $tableNames
+    ): void {
+        $tableName = $tableNames->hierarchyRelation();
+        $id = $this->getDatabaseIdentifier();
+        try {
+            $databaseConnection->executeStatement(
+                <<<SQL
+                    WITH updated AS (
+                        UPDATE {$tableName}
+                        SET childnodeanchors = array_remove(childnodeanchors, :childnodeanchor_to_remove),
+                            subtreetags = subtreetags - :childnodeanchor_to_remove_text
+                        WHERE contentstreamid = :contentstreamid
+                          AND parentnodeanchor = :parentnodeanchor
+                          AND dimensionspacepointhash = :dimensionspacepointhash
+                        RETURNING contentstreamid, parentnodeanchor, dimensionspacepointhash, childnodeanchors
+                    )
+                    DELETE FROM {$tableName} h
+                    USING updated AS u
+                    WHERE h.contentstreamid = u.contentstreamid
+                      AND h.parentnodeanchor = u.parentnodeanchor
+                      AND h.dimensionspacepointhash = u.dimensionspacepointhash
+                      AND array_length(u.childnodeanchors, 1) IS NULL
+                SQL,
+                [
+                    'contentstreamid' => $id['contentstreamid'],
+                    'parentnodeanchor' => $id['parentnodeanchor'],
+                    'dimensionspacepointhash' => $id['dimensionspacepointhash'],
+                    'childnodeanchor_to_remove' => $childNodeAnchor->value,
+                    'childnodeanchor_to_remove_text' => (string)$childNodeAnchor->value,
+                ]
+            );
+        } catch (DBALException $e) {
+            throw new \RuntimeException(sprintf('Failed to remove child node anchor from hierarchy relation: %s', $e->getMessage()), 1716484900, $e);
+        }
+    }
+
+    public function removeFromDatabase(Connection $databaseConnection, ContentGraphTableNames $tableNames): void
+    {
+        try {
+            $databaseConnection->delete($tableNames->hierarchyRelation(), $this->getDatabaseIdentifier());
+        } catch (DBALException $e) {
+            throw new \RuntimeException(sprintf('Failed to remove hierarchy relation from database: %s', $e->getMessage()), 1716484910, $e);
+        }
+    }
 }