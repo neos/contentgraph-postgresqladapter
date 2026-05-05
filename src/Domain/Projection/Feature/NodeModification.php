mhsdesign/Flowpack.ContentGraph.PostgreSQLAdapter

rollback Node CTEs to php based approach

src/Domain/Projection/Feature/NodeModification.php
@@ -15,9 +15,9 @@ declare(strict_types=1);
 namespace Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\Feature;
 
 use Doctrine\DBAL\Connection;
-use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\EventCouldNotBeAppliedToContentGraph;
-use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\NodeRecord;
+use Flowpack\ContentGraph\PostgreSQLAdapter\ContentGraphTableNames;
 use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\ProjectionReadQueries;
+use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\ProjectionWriteQueries;
 use Neos\ContentRepository\Core\Feature\NodeModification\Event\NodePropertiesWereSet;
 
 /**
@@ -27,109 +27,103 @@ use Neos\ContentRepository\Core\Feature\NodeModification\Event\NodePropertiesWer
  */
 trait NodeModification
 {
-    use CopyOnWrite;
-
     /**
      * @throws \Throwable
      */
     private function whenNodePropertiesWereSet(NodePropertiesWereSet $event): void
     {
+        // 1. Find the existing node by origin
+        $sourceNode = $this->getReadQueries()->findNodeRecordByOrigin(
+            $event->contentStreamId,
+            $event->originDimensionSpacePoint,
+            $event->nodeAggregateId
+        );
+        if ($sourceNode === null) {
+            throw new \RuntimeException(
+                sprintf(
+                    'Cannot apply NodePropertiesWereSet: node "%s" not found at origin %s in content stream %s',
+                    $event->nodeAggregateId->value,
+                    $event->originDimensionSpacePoint->toJson(),
+                    $event->contentStreamId->value
+                ),
+                1716498800
+            );
+        }
 
-        // copy on write
-        $parameters = [
-            'contentstreamid' => $event->contentStreamId->value,
-            'nodeaggregateid' => $event->nodeAggregateId->value,
-            'origindimensionspacepointhash' => $event->originDimensionSpacePoint->hash,
-            'propertievalues' => json_encode($event->propertyValues->jsonSerialize()),
-            'propertiestounset' => json_encode($event->propertiesToUnset->jsonSerialize())
-        ];
+        // 2. Compute the updated properties
+        $updatedProperties = $sourceNode->properties
+            ->merge($event->propertyValues)
+            ->unsetProperties($event->propertiesToUnset);
 
-        $query = <<<SQL
-            with existing_node as (
-                    select
-                        {$this->tableNames->functionGetRelationAnchorPoint()}(
-                            :nodeaggregateid,
-                            :contentstreamid,
-                            :origindimensionspacepointhash
-                        ) as relationanchorpoint
-                ),
-                is_copy_necessary as (
-                    select
-                        (select count(distinct h.contentstreamid)
-                         from {$this->tableNames->hierarchyRelation()} h, existing_node en
-                         where en.relationanchorpoint = any(h.childnodeanchors))
-                            > 1 as has_more_than_one_contentstream
-                ),
-                -- no copy required, since there are no other content streams
-                update_single_instance as (
-                    update {$this->tableNames->node()}
-                    set
-                        properties = (properties
-                                          -- first, remove the properties to unset
-                                          - coalesce((select array_agg(el.propertyname)
-                                             from jsonb_array_elements_text(:propertiestounset) el(propertyname)), array[]::text[])
-                                     )
-                                          -- then, add the new properties
-                                         || :propertievalues
-                    from existing_node en, is_copy_necessary icn
-                    where {$this->tableNames->node()}.relationanchorpoint = en.relationanchorpoint
-                      -- only, if there is exactly one contentstream
-                      and not icn.has_more_than_one_contentstream
-                ),
-                -- FIXME the whole copy on write might be a reusable function
-                copy_node_on_write as (
-                    insert into {$this->tableNames->node()}
-                        (nodeaggregateid, origindimensionspacepoint, origindimensionspacepointhash,
-                         nodetypename, classification, nodename, properties)
-                    select
-                        sn.nodeaggregateid,
-                        sn.origindimensionspacepoint,
-                        sn.origindimensionspacepointhash,
-                        sn.nodetypename,
-                        sn.classification,
-                        sn.nodename,
-                        -- copy the properties
-                        (sn.properties
-                              -- first, remove the properties to unset
-                              - coalesce((select array_agg(el.propertyname)
-                                 from jsonb_array_elements_text(:propertiestounset) el(propertyname)), array[]::text[])
-                        )
-                           -- then, add the new properties
-                           || :propertievalues
-                    -- source node
-                    from {$this->tableNames->node()} sn, existing_node en, is_copy_necessary icn
-                    where sn.relationanchorpoint = en.relationanchorpoint
-                      -- only, if there is more than one contentstream
-                      and icn.has_more_than_one_contentstream
-                    returning {$this->tableNames->node()}.relationanchorpoint
-                ),
-                reassign_ingoing_hierarchy_relations as (
-                    update {$this->tableNames->hierarchyRelation()}
-                       set childnodeanchors = array_replace(
-                              {$this->tableNames->hierarchyRelation()}.childnodeanchors,
-                              en.relationanchorpoint,
-                              cn.relationanchorpoint)
-                    from copy_node_on_write cn, existing_node en, is_copy_necessary icn
-                    where en.relationanchorpoint = any({$this->tableNames->hierarchyRelation()}.childnodeanchors)
-                      and {$this->tableNames->hierarchyRelation()}.contentstreamid = :contentstreamid
-                      -- only, if there is more than one contentstream
-                      and icn.has_more_than_one_contentstream
-                ),
-                reassign_outgoing_hierarchy_relations as (
-                    update {$this->tableNames->hierarchyRelation()}
-                       set parentnodeanchor = cn.relationanchorpoint
-                    from copy_node_on_write cn, existing_node en, is_copy_necessary icn
-                    where en.relationanchorpoint = {$this->tableNames->hierarchyRelation()}.parentnodeanchor
-                      and {$this->tableNames->hierarchyRelation()}.contentstreamid = :contentstreamid
-                      -- only, if there is more than one contentstream
-                      and icn.has_more_than_one_contentstream
-                )
-                -- TODO copy references
-            select 1
-        SQL;
+        // 3. Check if copy-on-write is needed
+        $numberOfContentStreams = $this->getReadQueries()
+            ->countContentStreamCoverage($sourceNode->relationAnchorPoint);
 
-        $this->getDatabaseConnection()->executeQuery($query, $parameters);
+        if ($numberOfContentStreams <= 1) {
+            // No copy needed — update the node record directly
+            $sourceNode->properties = $updatedProperties;
+            $this->getWriteQueries()->updateNodeRecord(
+                $this->getDatabaseConnection(),
+                $sourceNode
+            );
+        } else {
+            // Copy on write: create a new node record with the updated properties
+            $newAnchor = $this->getWriteQueries()->insertNodeRecord(
+                $this->getDatabaseConnection(),
+                $sourceNode->nodeAggregateId,
+                $sourceNode->originDimensionSpacePoint,
+                $updatedProperties,
+                $sourceNode->nodeTypeName,
+                $sourceNode->classification,
+                $sourceNode->nodeName
+            );
+
+            // Reassign ingoing hierarchy relations for this content stream
+            foreach (
+                $this->getReadQueries()->findIngoingHierarchyHyperrelationRecords(
+                    $event->contentStreamId,
+                    $sourceNode->relationAnchorPoint
+                ) as $ingoingRelation
+            ) {
+                $ingoingRelation->replaceChildNodeAnchor(
+                    $sourceNode->relationAnchorPoint,
+                    $newAnchor,
+                    $this->getDatabaseConnection(),
+                    $this->getTableNames()
+                );
+            }
+
+            // Reassign outgoing hierarchy relations for this content stream
+            foreach (
+                $this->getReadQueries()->findOutgoingHierarchyHyperrelationRecords(
+                    $event->contentStreamId,
+                    $sourceNode->relationAnchorPoint
+                ) as $outgoingRelation
+            ) {
+                $this->getWriteQueries()->replaceParentNodeAnchorOnHierarchyRecord(
+                    $this->getDatabaseConnection(),
+                    $outgoingRelation->getDatabaseIdentifier(),
+                    $newAnchor
+                );
+            }
+
+            // Copy reference relations
+            foreach (
+                $this->getReadQueries()->findOutgoingReferenceHyperrelationRecords(
+                    $sourceNode->relationAnchorPoint
+                ) as $referenceRelation
+            ) {
+                $copiedReference = $referenceRelation->withSourceNodeAnchor($newAnchor);
+                $this->getWriteQueries()->addReferenceToDatabase(
+                    $this->getDatabaseConnection(),
+                    $copiedReference
+                );
+            }
+        }
     }
 
     abstract protected function getDatabaseConnection(): Connection;
+    abstract protected function getReadQueries(): ProjectionReadQueries;
+    abstract protected function getWriteQueries(): ProjectionWriteQueries;
+    abstract protected function getTableNames(): ContentGraphTableNames;
 }

neos/contentgraph-postgresqladapter

src/Domain/Projection/Feature/NodeModification.php
@@ -15,9 +15,9 @@ declare(strict_types=1);
 namespace Neos\ContentGraph\PostgreSQLAdapter\Domain\Projection\Feature;
 
 use Doctrine\DBAL\Connection;
-use Neos\ContentGraph\PostgreSQLAdapter\Domain\Projection\EventCouldNotBeAppliedToContentGraph;
-use Neos\ContentGraph\PostgreSQLAdapter\Domain\Projection\NodeRecord;
+use Neos\ContentGraph\PostgreSQLAdapter\ContentGraphTableNames;
 use Neos\ContentGraph\PostgreSQLAdapter\Domain\Projection\ProjectionReadQueries;
+use Neos\ContentGraph\PostgreSQLAdapter\Domain\Projection\ProjectionWriteQueries;
 use Neos\ContentRepository\Core\Feature\NodeModification\Event\NodePropertiesWereSet;
 
 /**
@@ -27,109 +27,103 @@ use Neos\ContentRepository\Core\Feature\NodeModification\Event\NodePropertiesWer
  */
 trait NodeModification
 {
-    use CopyOnWrite;
-
     /**
      * @throws \Throwable
      */
     private function whenNodePropertiesWereSet(NodePropertiesWereSet $event): void
     {
+        // 1. Find the existing node by origin
+        $sourceNode = $this->getReadQueries()->findNodeRecordByOrigin(
+            $event->contentStreamId,
+            $event->originDimensionSpacePoint,
+            $event->nodeAggregateId
+        );
+        if ($sourceNode === null) {
+            throw new \RuntimeException(
+                sprintf(
+                    'Cannot apply NodePropertiesWereSet: node "%s" not found at origin %s in content stream %s',
+                    $event->nodeAggregateId->value,
+                    $event->originDimensionSpacePoint->toJson(),
+                    $event->contentStreamId->value
+                ),
+                1716498800
+            );
+        }
 
-        // copy on write
-        $parameters = [
-            'contentstreamid' => $event->contentStreamId->value,
-            'nodeaggregateid' => $event->nodeAggregateId->value,
-            'origindimensionspacepointhash' => $event->originDimensionSpacePoint->hash,
-            'propertievalues' => json_encode($event->propertyValues->jsonSerialize()),
-            'propertiestounset' => json_encode($event->propertiesToUnset->jsonSerialize())
-        ];
+        // 2. Compute the updated properties
+        $updatedProperties = $sourceNode->properties
+            ->merge($event->propertyValues)
+            ->unsetProperties($event->propertiesToUnset);
 
-        $query = <<<SQL
-            with existing_node as (
-                    select
-                        {$this->tableNames->functionGetRelationAnchorPoint()}(
-                            :nodeaggregateid,
-                            :contentstreamid,
-                            :origindimensionspacepointhash
-                        ) as relationanchorpoint
-                ),
-                is_copy_necessary as (
-                    select
-                        (select count(distinct h.contentstreamid)
-                         from {$this->tableNames->hierarchyRelation()} h, existing_node en
-                         where en.relationanchorpoint = any(h.childnodeanchors))
-                            > 1 as has_more_than_one_contentstream
-                ),
-                -- no copy required, since there are no other content streams
-                update_single_instance as (
-                    update {$this->tableNames->node()}
-                    set
-                        properties = (properties
-                                          -- first, remove the properties to unset
-                                          - coalesce((select array_agg(el.propertyname)
-                                             from jsonb_array_elements_text(:propertiestounset) el(propertyname)), array[]::text[])
-                                     )
-                                          -- then, add the new properties
-                                         || :propertievalues
-                    from existing_node en, is_copy_necessary icn
-                    where {$this->tableNames->node()}.relationanchorpoint = en.relationanchorpoint
-                      -- only, if there is exactly one contentstream
-                      and not icn.has_more_than_one_contentstream
-                ),
-                -- FIXME the whole copy on write might be a reusable function
-                copy_node_on_write as (
-                    insert into {$this->tableNames->node()}
-                        (nodeaggregateid, origindimensionspacepoint, origindimensionspacepointhash,
-                         nodetypename, classification, nodename, properties)
-                    select
-                        sn.nodeaggregateid,
-                        sn.origindimensionspacepoint,
-                        sn.origindimensionspacepointhash,
-                        sn.nodetypename,
-                        sn.classification,
-                        sn.nodename,
-                        -- copy the properties
-                        (sn.properties
-                              -- first, remove the properties to unset
-                              - coalesce((select array_agg(el.propertyname)
-                                 from jsonb_array_elements_text(:propertiestounset) el(propertyname)), array[]::text[])
-                        )
-                           -- then, add the new properties
-                           || :propertievalues
-                    -- source node
-                    from {$this->tableNames->node()} sn, existing_node en, is_copy_necessary icn
-                    where sn.relationanchorpoint = en.relationanchorpoint
-                      -- only, if there is more than one contentstream
-                      and icn.has_more_than_one_contentstream
-                    returning {$this->tableNames->node()}.relationanchorpoint
-                ),
-                reassign_ingoing_hierarchy_relations as (
-                    update {$this->tableNames->hierarchyRelation()}
-                       set childnodeanchors = array_replace(
-                              {$this->tableNames->hierarchyRelation()}.childnodeanchors,
-                              en.relationanchorpoint,
-                              cn.relationanchorpoint)
-                    from copy_node_on_write cn, existing_node en, is_copy_necessary icn
-                    where en.relationanchorpoint = any({$this->tableNames->hierarchyRelation()}.childnodeanchors)
-                      and {$this->tableNames->hierarchyRelation()}.contentstreamid = :contentstreamid
-                      -- only, if there is more than one contentstream
-                      and icn.has_more_than_one_contentstream
-                ),
-                reassign_outgoing_hierarchy_relations as (
-                    update {$this->tableNames->hierarchyRelation()}
-                       set parentnodeanchor = cn.relationanchorpoint
-                    from copy_node_on_write cn, existing_node en, is_copy_necessary icn
-                    where en.relationanchorpoint = {$this->tableNames->hierarchyRelation()}.parentnodeanchor
-                      and {$this->tableNames->hierarchyRelation()}.contentstreamid = :contentstreamid
-                      -- only, if there is more than one contentstream
-                      and icn.has_more_than_one_contentstream
-                )
-                -- TODO copy references
-            select 1
-        SQL;
+        // 3. Check if copy-on-write is needed
+        $numberOfContentStreams = $this->getReadQueries()
+            ->countContentStreamCoverage($sourceNode->relationAnchorPoint);
 
-        $this->getDatabaseConnection()->executeQuery($query, $parameters);
+        if ($numberOfContentStreams <= 1) {
+            // No copy needed — update the node record directly
+            $sourceNode->properties = $updatedProperties;
+            $this->getWriteQueries()->updateNodeRecord(
+                $this->getDatabaseConnection(),
+                $sourceNode
+            );
+        } else {
+            // Copy on write: create a new node record with the updated properties
+            $newAnchor = $this->getWriteQueries()->insertNodeRecord(
+                $this->getDatabaseConnection(),
+                $sourceNode->nodeAggregateId,
+                $sourceNode->originDimensionSpacePoint,
+                $updatedProperties,
+                $sourceNode->nodeTypeName,
+                $sourceNode->classification,
+                $sourceNode->nodeName
+            );
+
+            // Reassign ingoing hierarchy relations for this content stream
+            foreach (
+                $this->getReadQueries()->findIngoingHierarchyHyperrelationRecords(
+                    $event->contentStreamId,
+                    $sourceNode->relationAnchorPoint
+                ) as $ingoingRelation
+            ) {
+                $ingoingRelation->replaceChildNodeAnchor(
+                    $sourceNode->relationAnchorPoint,
+                    $newAnchor,
+                    $this->getDatabaseConnection(),
+                    $this->getTableNames()
+                );
+            }
+
+            // Reassign outgoing hierarchy relations for this content stream
+            foreach (
+                $this->getReadQueries()->findOutgoingHierarchyHyperrelationRecords(
+                    $event->contentStreamId,
+                    $sourceNode->relationAnchorPoint
+                ) as $outgoingRelation
+            ) {
+                $this->getWriteQueries()->replaceParentNodeAnchorOnHierarchyRecord(
+                    $this->getDatabaseConnection(),
+                    $outgoingRelation->getDatabaseIdentifier(),
+                    $newAnchor
+                );
+            }
+
+            // Copy reference relations
+            foreach (
+                $this->getReadQueries()->findOutgoingReferenceHyperrelationRecords(
+                    $sourceNode->relationAnchorPoint
+                ) as $referenceRelation
+            ) {
+                $copiedReference = $referenceRelation->withSourceNodeAnchor($newAnchor);
+                $this->getWriteQueries()->addReferenceToDatabase(
+                    $this->getDatabaseConnection(),
+                    $copiedReference
+                );
+            }
+        }
     }
 
     abstract protected function getDatabaseConnection(): Connection;
+    abstract protected function getReadQueries(): ProjectionReadQueries;
+    abstract protected function getWriteQueries(): ProjectionWriteQueries;
+    abstract protected function getTableNames(): ContentGraphTableNames;
 }