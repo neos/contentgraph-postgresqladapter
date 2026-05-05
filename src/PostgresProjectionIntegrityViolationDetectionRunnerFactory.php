mhsdesign/Flowpack.ContentGraph.PostgreSQLAdapter

FEATURE: ADD ProjcetionIntegrityViolationDetectionRunner for Postgres

src/PostgresProjectionIntegrityViolationDetectionRunnerFactory.php
@@ -1,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flowpack\ContentGraph\PostgreSQLAdapter;
+
+use Doctrine\DBAL\Connection;
+use Flowpack\ContentGraph\PostgreSQLAdapter\Domain\Projection\ProjectionIntegrityViolationDetector;
+use Neos\ContentRepository\Core\Factory\ContentRepositoryServiceFactoryDependencies;
+use Neos\ContentRepository\Core\Factory\ContentRepositoryServiceFactoryInterface;
+use Neos\ContentRepository\Core\Projection\ContentGraph\ProjectionIntegrityViolationDetectionRunner;
+
+/**
+ * @implements ContentRepositoryServiceFactoryInterface<ProjectionIntegrityViolationDetectionRunner>
+ * @internal
+ */
+class PostgresProjectionIntegrityViolationDetectionRunnerFactory implements ContentRepositoryServiceFactoryInterface
+{
+    public function __construct(
+        private readonly Connection $dbal,
+    ) {
+    }
+
+    public function build(
+        ContentRepositoryServiceFactoryDependencies $serviceFactoryDependencies
+    ): ProjectionIntegrityViolationDetectionRunner {
+        return new ProjectionIntegrityViolationDetectionRunner(
+            new ProjectionIntegrityViolationDetector(
+                $this->dbal,
+                ContentGraphTableNames::create(
+                    $serviceFactoryDependencies->contentRepositoryId
+                )
+            )
+        );
+    }
+}

neos/contentgraph-postgresqladapter

src/PostgresProjectionIntegrityViolationDetectionRunnerFactory.php
@@ -1,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Neos\ContentGraph\PostgreSQLAdapter;
+
+use Doctrine\DBAL\Connection;
+use Neos\ContentGraph\PostgreSQLAdapter\Domain\Projection\ProjectionIntegrityViolationDetector;
+use Neos\ContentRepository\Core\Factory\ContentRepositoryServiceFactoryDependencies;
+use Neos\ContentRepository\Core\Factory\ContentRepositoryServiceFactoryInterface;
+use Neos\ContentRepository\Core\Projection\ContentGraph\ProjectionIntegrityViolationDetectionRunner;
+
+/**
+ * @implements ContentRepositoryServiceFactoryInterface<ProjectionIntegrityViolationDetectionRunner>
+ * @internal
+ */
+class PostgresProjectionIntegrityViolationDetectionRunnerFactory implements ContentRepositoryServiceFactoryInterface
+{
+    public function __construct(
+        private readonly Connection $dbal,
+    ) {
+    }
+
+    public function build(
+        ContentRepositoryServiceFactoryDependencies $serviceFactoryDependencies
+    ): ProjectionIntegrityViolationDetectionRunner {
+        return new ProjectionIntegrityViolationDetectionRunner(
+            new ProjectionIntegrityViolationDetector(
+                $this->dbal,
+                ContentGraphTableNames::create(
+                    $serviceFactoryDependencies->contentRepositoryId
+                )
+            )
+        );
+    }
+}